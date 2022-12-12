cfc7d245078f4aa30c3a9f059a9b87472288d0f4
khanhnd52 <khanh.nd5252@gmail.com>
2022-12-12 16:29:13 +0700

    Add infrastructure

--- src/Examination/Examination.Domain/Aggregate/ExamAgregate/IExamRepository.cs
+++ src/Examination/Examination.Domain/Aggregate/ExamAgregate/IExamRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Examination.Domain.AggregateModels.ExamAggregate;
+using Examination.Domain.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace Examination.Domain.Aggregate.ExamAgregate
 {
-    internal interface IExamRepository
+    public interface IExamRepository : IRepositoryBase<Exam>
     {
+        Task<IEnumerable<Exam>> GetExamListAsync();
+        Task<Exam> GetExamByIdAsync(string id);
     }
 }
